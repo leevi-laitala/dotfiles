9c146f30754eaa50b7cd592d730ecf5cc9f8aaaa
Leevi Laitala <leevi.laitala@protonmail.com>
2021-08-22 15:15:42 +0300

    Updated polybar to work with laptop and desktop

--- config/polybar/scripts/launch.sh
+++ config/polybar/scripts/launch.sh
@@ -4,6 +4,8 @@ pkill -9 polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar thinkpad &
+#polybar thinkpad &
+polybar samsung4k &
+polybar shitty-aoc &
 
 echo "Bars launched..."
